b9d32075ed52c3508a936e72df91999187ed4d44
Ryan Tischer <ryantischer@protonmail.com>
2024-01-29 09:02:26 -0600

    licenses fix

--- example_login.py
+++ example_login.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, Pensando Systems
+# Copyright (c) 2024, AND
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
